ca340d885c252fe3e461e7d2b515a96ebad457c1
diego5x <deigo555g@gmail.com>
2025-11-23 21:19:30 -0300

    test: adiciona suíte completa de testes TDD para todos os endpoints da API

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,12 @@ const app = express();
 const conversaoRoutes = require('./routes/conversaoRoutes');
 
 app.use(express.json());
-
 app.use('/api', conversaoRoutes);
 
-app.listen(8080, () => {
-    console.log('Rodando em http://localhost:8080');
-});
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(8080, () => {
+        console.log('Rodando em http://localhost:8080');
+    });
+}
+
+module.exports = app; 
